cef3ecae458cad191dc73f438752a140ceda2b2e
shuxiaokai <2581105856@qq.com>
2020-08-19 18:01:55 +0800

    feat: 更新自定义联想输入

--- src/renderer/components/common/v-input/g-v-input.vue
+++ src/renderer/components/common/v-input/g-v-input.vue
@@ -6,11 +6,30 @@
 */
 <template>
     <div class="v-input" :class="{'valid-error': error}">
-        <el-input :value="value" v-bind="$attrs"  v-on="$listeners">
+        <el-input v-if="!remote" :value="value" v-bind="$attrs"  v-on="$listeners">
             <template slot="prepend">
                 <slot name="prepend"/>
             </template>
         </el-input>
+        <el-autocomplete 
+                v-else :value="value"
+                v-bind="$attrs"
+                value-key="key"
+                popper-class="g-v-autocomplete"
+                v-on="$listeners"
+                :fetch-suggestions="querySearchAsync"
+                :trigger-on-focus="false"
+                :highlight-first-item="true"
+                @select="handleSelect"
+                @input="handleAutocompleteInput"
+        >
+
+            <template slot-scope="{ item }">
+                <!-- <div>{{ item.key }}</div> -->
+                <s-emphasize :value="item.key" :keyword="currentQuerystring" class="d-flex"></s-emphasize>
+                <span class="f-sm gray-500">{{ item.type }}</span>
+            </template>
+        </el-autocomplete>
         <span v-show="error" class="error-tip">
             <span v-if="tip">{{ tip.message ? tip.message : tip }}</span>
             <span v-show="tip && tip.reference" class="theme-color ml-2" @click="handleJumpToStander">查看规范</span>
@@ -36,24 +55,68 @@ export default {
             type: Boolean,
             default: false
         },
+        remote: { //---------是否远程搜索
+            type: Boolean,
+            default: false
+        }
     },
     data() {
         return {
-
+            mindParams: [], //-------------联想参数
+            currentQuerystring: "", //-----当前键入的查询字符串
+            couldGetMindParams: false, //--是否允许联想查询
+            cancelTimer: null
         };
     },
-    created() {
-
+    mounted() {
+        
     },
     methods: {
-        //=====================================获取远程数据==================================//
-
-        //=====================================前后端交互====================================//
-
+        //初始化联想参数
+        initMindParams() {
+            const projectId = this.$route.query.id;
+            let reqMindParams = localStorage.getItem("pages/presetParams/request") || "{}";
+            let resMindParams = localStorage.getItem("pages/presetParams/response") || "{}";
+            reqMindParams = JSON.parse(reqMindParams);
+            resMindParams = JSON.parse(resMindParams);
+            reqMindParams[projectId] || (reqMindParams[projectId] = []) 
+            resMindParams[projectId] || (resMindParams[projectId] = []) 
+            this.mindParams = [...reqMindParams[projectId], ...resMindParams[projectId]];
+        },
         //=====================================组件间交互====================================//  
         //查看规范
         handleJumpToStander() {
             window.open(this.tip.reference)
+        },
+        //联想输入
+        querySearchAsync(queryString, cb) {
+            // if (!this.couldGetMindParams) { //必须在输入的时候才允许联想
+            //     cb([]);
+            // }
+            this.currentQuerystring = queryString;
+            if (this.mindParams.length === 0) {
+                this.initMindParams();
+            }
+            const matchedParams = this.mindParams.filter(val => val.key.toLocaleLowerCase().includes(queryString))
+            // console.log(matchedParams, this.mindParams, queryString)
+            if (queryString.trim() === "") {
+                cb([]);
+            } else if (matchedParams.length > 0) {
+                cb(matchedParams);
+            } else {
+                cb([])
+            }
+        },
+        //选择
+        handleSelect(val) {
+            this.$emit("mindParamsSelect", val);
+        },
+        handleAutocompleteInput() {
+            // this.couldGetMindParams = true;
+            // clearTimeout(this.cancelTimer);
+            // this.cancelTimer = setTimeout(() => {
+            //     this.couldGetMindParams = false;
+            // }, 1000)
         },
         //=====================================其他操作=====================================//
 
@@ -90,6 +153,12 @@ export default {
         100% {
             background: $white;
         }
+    }
+}
+.g-v-autocomplete {
+    li {
+        line-height: 1.3;
+        margin-bottom: size(10);
     }
 }
 </style>
