df3ddc0a043268f0d89e82feaf23d94792802b3f
shuxiaokai <2581105856@qq.com>
2020-08-21 17:39:24 +0800

    feat: 取消测试状态

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,12 @@
 <template>
     <div id="app">
-        <!-- <router-view></router-view> -->
-        <el-button @click="send">发送</el-button>
-        <el-button @click="stop">停止</el-button>
+        <router-view></router-view>
     </div>
 </template>
  
 <script>
 import { ipcRenderer  } from "electron";
-import HttpClient from "@/api/net.js"
-const httpClient = new HttpClient();
+
 export default {
     mounted() {
         window.addEventListener("keyup", (e) => {
@@ -19,22 +16,6 @@ export default {
                 ipcRenderer.send("vue-fresh-content")
             }
         })
-    },
-    methods: {
-        send() {
-            httpClient.request("http://127.0.0.1:7004/api/security/user_list", {
-                method: "get",
-                
-            }).then(res => {
-                console.log(res);
-            }).catch(err => {
-                console.error(err);
-            });
-            // console.log(curl)
-        },
-        stop() {
-            httpClient.stop();
-        },
     },
 };
 </script>
