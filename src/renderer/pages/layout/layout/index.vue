072c1af882e0fdc9ed810eef5682fe3850ea8ecd
shuxiaokai <2581105856@qq.com>
2020-09-21 16:32:04 +0800

    feat: 当真实姓名不存在时以手机号代替显示

--- src/renderer/pages/layout/layout/index.vue
+++ src/renderer/pages/layout/layout/index.vue
@@ -33,7 +33,7 @@
                 </div>
                 <el-dropdown>
                     <span class="cursor-pointer">
-                        <span>{{ userInfo.realName }}</span>
+                        <span>{{ userInfo.realName || userInfo.phone }}</span>
                         <i class="el-icon-arrow-down el-icon--right"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
